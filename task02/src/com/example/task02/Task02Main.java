a7fb28ac5e701f85a8106842e7bda7b813ee4a7f
Lirihiwa <lirihiwa@gmail.com>
2024-12-12 18:12:16 +0500

    task 2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,22 +1,35 @@
 package com.example.task02;
 
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Task02Main {
 
     public static void main(String[] args) {
 
-        /*
-        cycleGrayCode(2)
+        cycleGrayCode(1)
                 .limit(10)
                 .forEach(System.out::println);
-        */
 
     }
 
     public static IntStream cycleGrayCode(int n) {
 
-        return null; // your implementation here
+        if (n < 1 || n > 16) {
+            throw new IllegalArgumentException("n должно быть в диапазоне [1,16]");
+        }
+
+        //определяем длину массива как 2^n
+        int size = 1 << n;
+
+        //создаем массив по правилу Грея
+        int[] grayCodes = IntStream.range(0, size)
+                .map(i -> i ^ (i >> 1))
+                .toArray();
+
+        //нормализуем данные из стрима и сопоставляем с кодом Грея
+        return IntStream.iterate(0, i -> (i + 1) % grayCodes.length)
+                .map(i -> grayCodes[i]);
 
     }
 
